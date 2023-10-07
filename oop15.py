748bd25b7ebb5a3f65c0c2141c323aee39eb5c7b
Antoshkin <milka.vilka.007@gmail.com>
2023-10-07 17:22:46 +0300

    17

--- oop15.py
+++ oop15.py
@@ -1,8 +1,13 @@
+import csv
+
+class EmailAlreadyExistsException(Exception):
+    pass
 class Employee:
-    email = ""
-    def __init__(self, name, salary):
+    def __init__(self, name, salary, email):
         self.name = name
         self.salary = salary
+        self.email = email
+        self.save_email()
 
     def work(self):
         return "I come to the office."
@@ -22,10 +27,22 @@ class Employee:
     def __le__(self, other):
         return self.salary <= other.salary
 
+    def save_email(self):
+        try:
+            self.validate()
+            with open("emails.csv", "a") as f:
+                f.write(f"{self.email} \n")
+        except EmailAlreadyExistsException:
+            print("ERROR. The email is already recorded in the file")
+    def validate (self):
+        with open("emails.csv") as f:
+            for line in f:
+                if self.email in line:
+                    raise EmailAlreadyExistsException
 
 class Developer(Employee):
-    def __init__(self, name, salary, tech_stack):
-        super().__init__(name, salary)
+    def __init__(self, name, salary, email, tech_stack):
+        super().__init__(name, salary, email)
         self.tech_stack = tech_stack
 
     def work(self):
@@ -37,10 +54,11 @@ class Developer(Employee):
     def __add__(self, other):
         return Developer(
             self.name + " " + other.name,
+            max(self.salary, other.salary),
+            self.email + other.email,
             set(self.tech_stack + other.tech_stack),
-            max(self.salary, other.salary)
-        )
 
+        )
 
 class Recruiter(Employee):
     def work(self):
@@ -50,17 +68,17 @@ class Recruiter(Employee):
         return self.__class__.__name__
 
 
-michael = Developer("Michael",1500, ("Java", "JS", "C++"))
+michael = Developer("Michael",1500,"1", ("Java", "JS", "C++"))
 print(michael)
 print("Salary calculation Michael in 10 days =", michael.check_salary(10))
 print("Michael tech stack:", michael.tech_stack)
 
-alisa = Developer("Alisa", 2000, ("PHP", "Java"))
+alisa = Developer("Alisa", 2000, "gmail", ("PHP", "Java"))
 print("Salary Alisa less salary Michael:", alisa.salary < michael.salary)
 print("Quantity tech stack Alisa less tech stack Michael:",
       len(alisa.tech_stack) < len(michael.tech_stack))
 
-emma = Recruiter("Emma", 1000)
+emma = Recruiter("Emma",   1000, "ukrmail")
 print(emma)
 print("Salary calculation Emma in 10 days =", emma.check_salary(10))
 
@@ -68,7 +86,6 @@ developer_sum = michael + alisa
 print(
     developer_sum.name,
     developer_sum.salary,
-    developer_sum.tech_stack
+    developer_sum.tech_stack,
+    developer_sum.email
 )
-
-
